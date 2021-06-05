fe3c03f4f83a6ad31868d30de67f4b8e1c62e44e
Eddaga <kyuyong@esl.mju.ac.kr>
2021-06-05 21:33:06 +0900

    Create motor_test.ino

---
+++ motor/motor_test/motor_test.ino
@@ -0,0 +1,20 @@
+#define D1 5
+#define D2 4
+#define D0 0
+
+void setup() {
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D0, OUTPUT);
+}
+
+void loop() 
+{
+  analogWrite(D0,500);  
+  digitalWrite(D1,HIGH);
+  digitalWrite(D2,LOW);
+  delay(1000);
+  digitalWrite(D1,LOW);
+  digitalWrite(D2,HIGH);
+  delay(1000);
+}
